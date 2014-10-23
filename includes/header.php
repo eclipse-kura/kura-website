b7e104f3a91d8786b468acd1b35bedbfb3ba9c44
David Woodard <david.woodard@eurotech.com>
2014-10-23 10:54:23 -0700

    Moved bootstrap references to CDN to clear error messages.

--- includes/header.php
+++ includes/header.php
@@ -19,14 +19,14 @@
     <link href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
 	<link rel="shortcut icon" href="images/favicon.png">
 
-	<link href="css/bootstrap.min.css" rel="stylesheet" type='text/css'>
+	<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet" type='text/css'>
 	<link href="css/style.css" rel="stylesheet" type="text/css">
 
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
 	<script>
 		$.noConflict();
 	</script>
-	<script src="js/bootstrap.min.js"></script>
+	<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
 	<!-- Google Analytics -->
 	<script>
 	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
