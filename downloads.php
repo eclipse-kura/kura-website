e2c89f0c05ca67d61749de42fbd9cbe0837f54ec
David Woodard <david.woodard@eurotech.com>
2014-12-19 15:05:13 -0800

    Modifications for Kura 1.1.0 release.

--- downloads.php
+++ downloads.php
@@ -3,13 +3,19 @@
   <div class="row">
       <div class="col-md-6">
           <h2>Kura Downloads</h2>
-          <p>The below downloads are distributed through the Eclipse Foundation and meet all requirements of the <a href="https://www.eclipse.org/legal/epl-v10.html" target="_blank">Eclipse Public License</a>.</p>
+          <p>The below downloads are distributed through the Eclipse Foundation and meet all requirements of the <a href="https://www.eclipse.org/legal/epl-v10.html" target="_blank">Eclipse Public License</a>.
+            Entries below the downloads section give more explanation on each type of download.</p>
           <ul>
-              <li style="color: #4f5c6d"><a href="http://www.eclipse.org/downloads/download.php?file=/kura/releases/1.0.0/kura_1.0.0_raspberry-pi_armv6.deb" target="_blank">Raspbian - Stable</a></li>
-              <li style="color: #4f5c6d"><a href="http://www.eclipse.org/downloads/download.php?file=/kura/SNAPSHOT/kura-raspberry-pi_1.0.0-SNAPSHOT_armv6.deb" target="_blank">Raspbian - Development</a></li>
-              <li style="color: #4f5c6d"><a href="http://www.eclipse.org/downloads/download.php?file=/kura/releases/1.0.0/kura_1.0.0_beaglebone_debian_armv7.deb" target="_blank">BeagleBone - Stable</a></li></li>
-              <li style="color: #4f5c6d"><a href="http://www.eclipse.org/downloads/download.php?file=/kura/SNAPSHOT/kura-beaglebone_debian_1.0.0-SNAPSHOT_armv7.deb" target="_blank">BeagleBone - Development</a></li>
-              <li style="color: #4f5c6d"><a href="http://www.eclipse.org/downloads/download.php?file=/kura/releases/1.0.0/user_workspace_archive_1.0.0.zip" target="_blank">Developer's Workspace</a></li>
+              <li style="color: #4f5c6d"><a href="http://www.eclipse.org/downloads/download.php?file=/kura/releases/1.1.0/kura_1.1.0_raspberry-pi_armv6.deb" target="_blank">Raspbian - Stable</a></li>
+              <li style="color: #4f5c6d"><a href="http://www.eclipse.org/downloads/download.php?file=/kura/releases/1.1.0/kura_1.1.0_raspberry-pi-nn_armv6.deb" target="_blank">Raspbian (No Net)- Stable</a></li>
+              <li style="color: #4f5c6d"><a href="http://www.eclipse.org/downloads/download.php?file=/kura/SNAPSHOT/kura_1.1.0-SNAPSHOT_raspberry-pi_armv6.deb" target="_blank">Raspbian - Development</a></li>
+              <li style="color: #4f5c6d"><a href="http://www.eclipse.org/downloads/download.php?file=/kura/releases/1.1.0/kura_1.1.0_raspberry-pi-bplus_armv6.deb" target="_blank">Raspbian (Model B+)- Stable</a></li>
+              <li style="color: #4f5c6d"><a href="http://www.eclipse.org/downloads/download.php?file=/kura/releases/1.1.0/kura_1.1.0_raspberry-pi-bplus-nn_armv6.deb" target="_blank">Raspbian (Model B+, No Net)- Stable</a></li>
+              <li style="color: #4f5c6d"><a href="http://www.eclipse.org/downloads/download.php?file=/kura/SNAPSHOT/kura_1.1.0-SNAPSHOT_raspberry-pi-bplus_armv6.deb" target="_blank">Raspbian (Model B+)- Development</a></li>
+              <li style="color: #4f5c6d"><a href="http://www.eclipse.org/downloads/download.php?file=/kura/releases/1.1.0/kura_1.1.0_beaglebone_debian_armv7.deb" target="_blank">BeagleBone - Stable</a></li></li>
+              <li style="color: #4f5c6d"><a href="http://www.eclipse.org/downloads/download.php?file=/kura/releases/1.1.0/kura_1.1.0_beaglebone-nn_debian_armv7.deb" target="_blank">BeagleBone (No Net) - Stable</a></li></li>
+              <li style="color: #4f5c6d"><a href="http://www.eclipse.org/downloads/download.php?file=/kura/SNAPSHOT/kura_1.1.0-SNAPSHOT_beaglebone_debian_armv7.deb" target="_blank">BeagleBone - Development</a></li>
+              <li style="color: #4f5c6d"><a href="http://www.eclipse.org/downloads/download.php?file=/kura/releases/1.1.0/user_workspace_archive_1.1.0.zip" target="_blank">Developer's Workspace</a></li>
           </ul>
       </div>
       <div class="col-md-6">
@@ -18,9 +24,13 @@
             and <b>are not</b> covered by EPL.
           </p>
           <ul>
-              <li style="color: #4f5c6d"><a href="https://s3.amazonaws.com/kura_downloads/raspbian/release/1.0.0/kura_1.0.0_raspberry-pi_armv6.deb" target="_blank">Raspbian (with Web UI) - Stable</a></li>
-              <li style="color: #4f5c6d"><a href="https://s3.amazonaws.com/kura_downloads/debian/release/1.0.0/kura_1.0.0_beaglebone_debian_armv7.deb" target="_blank">BeagleBone (with Web UI) - Stable</a></li>
-              <li style="color: #4f5c6d"><a href="https://s3.amazonaws.com/kura_downloads/user_workspace/1.0.0/user_workspace_archive_1.0.0.zip" target="_blank">Developer's Workspace (with Web UI)</a></li>
+              <li style="color: #4f5c6d"><a href="https://s3.amazonaws.com/kura_downloads/raspbian/release/1.1.0/kura_1.1.0_raspberry-pi_armv6.deb" target="_blank">Raspbian (with Web UI) - Stable</a></li>
+              <li style="color: #4f5c6d"><a href="https://s3.amazonaws.com/kura_downloads/raspbian/release/1.1.0/kura_1.1.0_raspberry-pi-nn_armv6.deb" target="_blank">Raspbian (No, Net, with Web UI) - Stable</a></li>
+              <li style="color: #4f5c6d"><a href="https://s3.amazonaws.com/kura_downloads/raspbian/release/1.1.0/kura_1.1.0_raspberry-pi-bplus_armv6.deb" target="_blank">Raspbian (Model B+, with Web UI) - Stable</a></li>
+              <li style="color: #4f5c6d"><a href="https://s3.amazonaws.com/kura_downloads/raspbian/release/1.1.0/kura_1.1.0_raspberry-pi-bplus-nn_armv6.deb" target="_blank">Raspbian (Model B+, No Net, with Web UI) - Stable</a></li>
+              <li style="color: #4f5c6d"><a href="https://s3.amazonaws.com/kura_downloads/debian/release/1.1.0/kura_1.1.0_beaglebone_debian_armv7.deb" target="_blank">BeagleBone (with Web UI) - Stable</a></li>
+              <li style="color: #4f5c6d"><a href="https://s3.amazonaws.com/kura_downloads/debian/release/1.1.0/kura_1.1.0_beaglebone-nn_debian_armv7.deb" target="_blank">BeagleBone (No net, with Web UI) - Stable</a></li>
+              <li style="color: #4f5c6d"><a href="https://s3.amazonaws.com/kura_downloads/user_workspace/1.1.0/user_workspace_archive_1.1.0.zip" target="_blank">Developer's Workspace (with Web UI)</a></li>
               <li style="color: #4f5c6d"><a href="https://s3.amazonaws.com/kura_downloads/raspbian/snapshot/kura_1.1.0-SNAPSHOT_raspberry-pi_armv6.deb" target="_blank">Raspbian Snapshot Release</a></li>
           </ul>
       </div>
@@ -33,7 +43,8 @@
   <a name="debian_packages"></a>
   <h2>Raspbian/Debian Packages</h2>
   <p>
-    Installation instructions for the Raspberry Pi and BeagleBone Black can be found at the below locations:</p>
+    Along with release and snapshot versions, Kura also provides "no networking" versions of the Debian install files. Files with "(No Net)" will provide frameworks in which Kura will not assist in configuring
+    network interfaces or firewall. Installation instructions for the Raspberry Pi and BeagleBone Black can be found at the below locations:</p>
     <ul>
         <li style="color: #4f5c6d"><a href="http://eclipse.github.io/kura/doc/raspberry-pi-quick-start.html" target="_blank">Raspberry Pi Quick Start</a></li>
         <li style="color: #4f5c6d"><a href="http://eclipse.github.io/kura/doc/beaglebone-quick-start.html" target="_blank">BeagleBone Black Quick Start</a></li>
@@ -49,12 +60,13 @@
       <li style="color: #4f5c6d"><a href="https://repo.eclipse.org/content/repositories/kura-releases" target="_blank">Release</a></li>
       <li style="color: #4f5c6d"><a href="https://repo.eclipse.org/content/repositories/kura-snapshots/" target="_blank">SNAPSHOTS</a></li>
     </ul>
-  <h2>Other</h2>
+  <h2>Manual Installation</h2>
   <p>
-    Other artifacts of the Eclipse build system include:
+    If you want to manually install Kura on your Raspberry Pi or BeagleBone, the jar file archives can be found below.
     <ul>
-        <li style="color: #4f5c6d">The Raspberry jars. If you want to manually install Kura on your Raspberry, grab the archive of Kura
-           <a href="http://www.eclipse.org/downloads/download.php?file=/kura/releases/1.0.0/kura_1.0.0_raspberry-pi.zip" target="_blank">here</a></li>
+        <li style="color: #4f5c6d"><a href="http://www.eclipse.org/downloads/download.php?file=/kura/releases/1.1.0/kura_1.1.0_raspberry-pi.zip" target="_blank">Raspbian</a></li>
+        <li style="color: #4f5c6d"><a href="http://www.eclipse.org/downloads/download.php?file=/kura/releases/1.1.0/kura_1.1.0_raspberry-pi-bplus.zip" target="_blank">Raspbian (Model B+)</a></li>
+        <li style="color: #4f5c6d"><a href="http://www.eclipse.org/downloads/download.php?file=/kura/releases/1.1.0/kura_1.1.0_beaglebone.zip" target="_blank">BeagleBone</a></li>
     </ul>
   </p>
 </div>
--- index_old.php
+++ index_old.php
@@ -23,115 +23,45 @@
   </div>
 </div>
 <div id="features">
-  <div class="container features">
+  <div class="container">
     <div class="row">
-          <div class="col-md-12" style="position: relative;">
-			  <p class="feature-header">Solution Showcases</p>
-              <div class="row" style="padding-right:10px">
-				  <div class="col-md-2">
-					<img class="img-responsive featuresimg" style="height:80%;padding-top:15px" src="images/ETH-Cloud.png">
-				  </div>
-                  <div class="col-md-2">
-					<p class="feature-body"><a style="color:white;" href="http://www.eurotech.com/en/products/software+services/everyware+cloud+m2m+platform">
-					Cloud-based
-					Kura device management</a></p>
-                  </div>
-
-				  <div class="col-md-2 feature-box">
-					<img class="img-responsive featuresimg" style="height:50%;padding-top:10px" src="images/rpi256.png">
-				  </div>
-                  <div class="col-md-2"><br />
-					<p class="feature-body"><a style="color:white;" href="downloads.php">Shoot-a-Pi Tutorial</a></p>                             
-                  </div>
-                  
-                  <div class="col-md-2 feature-box">
-     				<img class="img-responsive featuresimg" style="height:120px!important;width:125px!important; " src="images/ETH-GreenHouse_ws.png">
-				  </div>
-                  <div class="col-md-2"><br />
-					<p class="feature-body"><a style="color:white;" href="http://iot.eclipse.org/java/tutorial/">Greenhouse Tutorial</a></p>                             
-                  </div>
-                  
-              </div>
-		  </div>				             
-    </div>
-     
-  </div>  
-  <br />
-  <div class="container features"> 
-	<p class="feature-header">M2M Gateways</p>   
-    <div class="row" style="margin: 25px 40px 0px 0px; padding: 25px 10px 0px 0px; border-top: 1px solid white;">
-      <div class="col-md-4 feature-box">
+      <div class="col-md-5 feature-box">
         <img class="img-responsive" src="images/rpi256.png">
       </div>
-      <div class="col-md-7">
+      <div class="col-md-6">
         <p class="feature-header">The Raspberry Pi Gateway</p>
         <p class="feature-body">Let Kura turn your Raspberry Pi into an IoT gateway. The Kura source
-          code is complete with example projects to get you up and running quickly. 
-          <a style="color: navy" href="downloads.php">Get the latest downloads</a>.</p>
+          code is complete with example projects to get you up and running quickly. Get the latest downloads
+          <a style="color: #e16036" href="downloads.php">here</a></p>
       </div>
     </div>
-    
-    <div class="row" style="margin: 25px 40px 0px 0px; padding: 25px 10px 0px 0px; border-top: 1px solid white;">
-      <div class="col-md-4 feature-box">
-        <img class="img-responsive" src="images/BBB_Alt_View_Small.png">
-      </div>		
-      <div class="col-md-7">
+    <div class="row" style="margin-top: 50px; padding-top: 50px; border-top: 1px solid white;">
+      <div class="col-md-6">
         <p class="feature-header">BeagleBone Black</p>
         <p class="feature-body">Kura is compatible with the BeagleBone Black Rev C.
-          <a style="color: navy" href="downloads.php">Get the latest Debian package</a>.</p>
+          Get the latest Debian package <a style="color: #e16036" href="downloads.php">here</a>.</p>
       </div>
-
+      <div class="col-md-5 feature-box">
+        <img class="img-responsive" src="images/BBB_Alt_View_Small.png">
+      </div>
     </div>
-    
-    
-    <div class="row" style="margin: 25px 40px 0px 0px; padding: 25px 10px 0px 0px; border-top: 1px solid white;">
-      <div class="col-md-4 feature-box">
+    <div class="row" style="margin-top: 50px; padding-top: 50px; border-top: 1px solid white;">
+      <div class="col-md-5 feature-box">
         <img class="img-responsive" src="images/eurotech.png">
       </div>
       <div class="col-md-6">
-        <p class="feature-header">Industral Gateways</p>
-        <p class="feature-body">Take a look at vendors who are using Kura in their 
-          <a style="color: navy" href="vendors.php">commercial gateways</a>.</p>
+        <p class="feature-header">Industrial Gateways</p>
+        <p class="feature-body">Take a look at vendors who are using Kura in their commercial
+          gateways <a style="color: #e16036" href="vendors.php">here</a>.</p>
       </div>
       </div>
-    </div>
-      
-
     </div>
   </div>
 </div>
-
-<div id="sidebar">
-  <div class="container">
-    <h2>Latest News</h2>
-    <div class="row">
-      <div class="col-md-3 col-md-offset1 box">
- 
-        <p class="article-link">
-          <a href="http://www.youtube.com/watch?v=PXlDJMK7yqg" target="_blank">Live Demo at EclipseCon 2014</a><br /><br />
-        </p>
-      </div>
-      <div class="col-md-3 col-md-offset3 box">
-        <p class="article-link">
-          <a href="http://www.parleys.com/play/543f8e75e4b06e1184ae4115" target="_blank">Video about end-to-end IoT solutions</a>
-        </p>
-      </div>
-        <div class="col-md-4 col-md-offset4 box">
-
-        <p class="article-link">
-          <a href="http://www.bitreactive.com/kura-config/" target="_blank">BitReactive: Configure IoT Gateway Applications with Kura</a>
-        </p>
-      </div>
-    </div>
-  </div>
-
-</div>
-
-
 <div id="articles">
   <div class="container">
     <div class="row">
-      <div class="col-md-4 col-md-offset4 article-box">
+      <div class="col-md-4 col-md-offset1 article-box">
         <p class="lead">
           <span class="quote lquote">&ldquo;</span>
           Deploying and configuring one device to act as a node in the Internet of Things is relatively easy. Doing the same for hundreds or thousands of devices is not so easy though. This is where the new Eclipse project Kura comes in.
