00855854378dbf8c63702ba6d1ee97bd0439aa9c
David Woodard <david.woodard@eurotech.com>
2015-04-10 15:40:47 -0700

    Update links for v1.1.2 release.

--- downloads.php
+++ downloads.php
@@ -2,38 +2,38 @@
 <div class="container" style="min-height: 800px;">
   <div class="row">
       <div class="col-md-6">
-          <h2>Kura 1.1.1 Downloads</h2>
-          <h4>Released on Feb 5th, 2015 - <a href="https://github.com/eclipse/kura/blob/develop/kura/distrib/RELEASE_NOTES.txt">Release Notes</a></h4>
+          <h2>Kura 1.1.2 Downloads</h2>
+          <h4>Released on Apr 10th, 2015 - <a href="https://github.com/eclipse/kura/blob/develop/kura/distrib/RELEASE_NOTES.txt">Release Notes</a></h4>
           <p>The below downloads are distributed through the Eclipse Foundation and meet all requirements of the <a href="https://www.eclipse.org/legal/epl-v10.html" target="_blank">Eclipse Public License</a>.
             Entries below the downloads section give more explanation on each type of download.</p>
           <ul>
-              <li style="color: #4f5c6d"><a href="http://www.eclipse.org/downloads/download.php?file=/kura/releases/1.1.1/kura_1.1.1_raspberry-pi_armv6.deb" target="_blank">Raspbian - Stable</a></li>
-              <li style="color: #4f5c6d"><a href="http://www.eclipse.org/downloads/download.php?file=/kura/releases/1.1.1/kura_1.1.1_raspberry-pi-nn_armv6.deb" target="_blank">Raspbian (No Net)- Stable</a></li>
+              <li style="color: #4f5c6d"><a href="http://www.eclipse.org/downloads/download.php?file=/kura/releases/1.1.2/kura_1.1.2_raspberry-pi_armv6.deb" target="_blank">Raspbian - Stable</a></li>
+              <li style="color: #4f5c6d"><a href="http://www.eclipse.org/downloads/download.php?file=/kura/releases/1.1.2/kura_1.1.2_raspberry-pi-nn_armv6.deb" target="_blank">Raspbian (No Net)- Stable</a></li>
               <li style="color: #4f5c6d"><a href="http://www.eclipse.org/downloads/download.php?file=/kura/SNAPSHOT/kura_1.2.0-SNAPSHOT_raspberry-pi_armv6.deb" target="_blank">Raspbian - Development</a></li>
-              <li style="color: #4f5c6d"><a href="http://www.eclipse.org/downloads/download.php?file=/kura/releases/1.1.1/kura_1.1.1_raspberry-pi-bplus_armv6.deb" target="_blank">Raspbian (Model B+)- Stable</a></li>
-              <li style="color: #4f5c6d"><a href="http://www.eclipse.org/downloads/download.php?file=/kura/releases/1.1.1/kura_1.1.1_raspberry-pi-bplus-nn_armv6.deb" target="_blank">Raspbian (Model B+, No Net)- Stable</a></li>
+              <li style="color: #4f5c6d"><a href="http://www.eclipse.org/downloads/download.php?file=/kura/releases/1.1.2/kura_1.1.2_raspberry-pi-bplus_armv6.deb" target="_blank">Raspbian (Model B+)- Stable</a></li>
+              <li style="color: #4f5c6d"><a href="http://www.eclipse.org/downloads/download.php?file=/kura/releases/1.1.2/kura_1.1.2_raspberry-pi-bplus-nn_armv6.deb" target="_blank">Raspbian (Model B+, No Net)- Stable</a></li>
               <li style="color: #4f5c6d"><a href="http://www.eclipse.org/downloads/download.php?file=/kura/SNAPSHOT/kura_1.2.0-SNAPSHOT_raspberry-pi-bplus_armv6.deb" target="_blank">Raspbian (Model B+)- Development</a></li>
-              <li style="color: #4f5c6d"><a href="http://www.eclipse.org/downloads/download.php?file=/kura/releases/1.1.1/kura_1.1.1_beaglebone_debian_armv7.deb" target="_blank">BeagleBone - Stable</a></li></li>
-              <li style="color: #4f5c6d"><a href="http://www.eclipse.org/downloads/download.php?file=/kura/releases/1.1.1/kura_1.1.1_beaglebone-nn_debian_armv7.deb" target="_blank">BeagleBone (No Net) - Stable</a></li></li>
+              <li style="color: #4f5c6d"><a href="http://www.eclipse.org/downloads/download.php?file=/kura/releases/1.1.2/kura_1.1.2_beaglebone_debian_armv7.deb" target="_blank">BeagleBone - Stable</a></li></li>
+              <li style="color: #4f5c6d"><a href="http://www.eclipse.org/downloads/download.php?file=/kura/releases/1.1.2/kura_1.1.2_beaglebone-nn_debian_armv7.deb" target="_blank">BeagleBone (No Net) - Stable</a></li></li>
               <li style="color: #4f5c6d"><a href="http://www.eclipse.org/downloads/download.php?file=/kura/SNAPSHOT/kura_1.2.0-SNAPSHOT_beaglebone_debian_armv7.deb" target="_blank">BeagleBone - Development</a></li>
-              <li style="color: #4f5c6d"><a href="http://www.eclipse.org/downloads/download.php?file=/kura/releases/1.1.1/user_workspace_archive_1.1.1.zip" target="_blank">Developer's Workspace</a></li>
+              <li style="color: #4f5c6d"><a href="http://www.eclipse.org/downloads/download.php?file=/kura/releases/1.1.2/user_workspace_archive_1.1.2.zip" target="_blank">Developer's Workspace</a></li>
           </ul>
       </div>
       <div class="col-md-6">
-          <h2>Kura 1.1.1 Extended Downloads</h2>
-          <h4>Released on Dec 22nd, 2014 - <a href="https://github.com/eclipse/kura/blob/develop/kura/distrib/RELEASE_NOTES.txt">Release Notes</a></h4>
+          <h2>Kura 1.1.2 Extended Downloads</h2>
+          <h4>Released on Apr 10th, 2015 - <a href="https://github.com/eclipse/kura/blob/develop/kura/distrib/RELEASE_NOTES.txt">Release Notes</a></h4>
           <p>The below downloads contain the Kura Web UI, the Open JDK Device I/O library to access GPIO, I2C, SPI and PWM, and the bundles to interface with the CAN bus. These features, while released under
             <a href="https://www.eclipse.org/legal/epl-v10.html" target="_blank">Eclipse Public License</a>, depend on third party libraries that cannot be distributed directly by Eclipse.
           </p>
           <ul>
-              <li style="color: #4f5c6d"><a href="https://s3.amazonaws.com/kura_downloads/raspbian/release/1.1.1/kura_1.1.1_raspberry-pi_armv6.deb" target="_blank">Raspbian (with Web UI) - Stable</a></li>
-              <li style="color: #4f5c6d"><a href="https://s3.amazonaws.com/kura_downloads/raspbian/release/1.1.1/kura_1.1.1_raspberry-pi-nn_armv6.deb" target="_blank">Raspbian (No, Net, with Web UI) - Stable</a></li>
-              <li style="color: #4f5c6d"><a href="https://s3.amazonaws.com/kura_downloads/raspbian/release/1.1.1/kura_1.1.1_raspberry-pi-bplus_armv6.deb" target="_blank">Raspbian (Model B+, with Web UI) - Stable</a></li>
-              <li style="color: #4f5c6d"><a href="https://s3.amazonaws.com/kura_downloads/raspbian/release/1.1.1/kura_1.1.1_raspberry-pi-bplus-nn_armv6.deb" target="_blank">Raspbian (Model B+, No Net, with Web UI) - Stable</a></li>
-              <li style="color: #4f5c6d"><a href="https://s3.amazonaws.com/kura_downloads/debian/release/1.1.1/kura_1.1.1_beaglebone_debian_armv7.deb" target="_blank">BeagleBone (with Web UI) - Stable</a></li>
-              <li style="color: #4f5c6d"><a href="https://s3.amazonaws.com/kura_downloads/debian/release/1.1.1/kura_1.1.1_beaglebone-nn_debian_armv7.deb" target="_blank">BeagleBone (No net, with Web UI) - Stable</a></li>
-              <li style="color: #4f5c6d"><a href="https://s3.amazonaws.com/kura_downloads/user_workspace/release/1.1.1/user_workspace_archive_1.1.1.zip" target="_blank">Developer's Workspace (with Web UI)</a></li>
-              <li style="color: #4f5c6d"><a href="https://s3.amazonaws.com/kura_downloads/raspbian/snapshot/kura_1.1.1-SNAPSHOT_raspberry-pi_armv6.deb" target="_blank">Raspbian Snapshot Release</a></li>
+              <li style="color: #4f5c6d"><a href="https://s3.amazonaws.com/kura_downloads/raspbian/release/1.1.2/kura_1.1.2_raspberry-pi_armv6.deb" target="_blank">Raspbian (with Web UI) - Stable</a></li>
+              <li style="color: #4f5c6d"><a href="https://s3.amazonaws.com/kura_downloads/raspbian/release/1.1.2/kura_1.1.2_raspberry-pi-nn_armv6.deb" target="_blank">Raspbian (No, Net, with Web UI) - Stable</a></li>
+              <li style="color: #4f5c6d"><a href="https://s3.amazonaws.com/kura_downloads/raspbian/release/1.1.2/kura_1.1.2_raspberry-pi-bplus_armv6.deb" target="_blank">Raspbian (Model B+, with Web UI) - Stable</a></li>
+              <li style="color: #4f5c6d"><a href="https://s3.amazonaws.com/kura_downloads/raspbian/release/1.1.2/kura_1.1.2_raspberry-pi-bplus-nn_armv6.deb" target="_blank">Raspbian (Model B+, No Net, with Web UI) - Stable</a></li>
+              <li style="color: #4f5c6d"><a href="https://s3.amazonaws.com/kura_downloads/debian/release/1.1.2/kura_1.1.2_beaglebone_debian_armv7.deb" target="_blank">BeagleBone (with Web UI) - Stable</a></li>
+              <li style="color: #4f5c6d"><a href="https://s3.amazonaws.com/kura_downloads/debian/release/1.1.2/kura_1.1.2_beaglebone-nn_debian_armv7.deb" target="_blank">BeagleBone (No net, with Web UI) - Stable</a></li>
+              <li style="color: #4f5c6d"><a href="https://s3.amazonaws.com/kura_downloads/user_workspace/release/1.1.2/user_workspace_archive_1.1.2.zip" target="_blank">Developer's Workspace (with Web UI)</a></li>
+              <li style="color: #4f5c6d"><a href="https://s3.amazonaws.com/kura_downloads/raspbian/snapshot/kura_1.1.2-SNAPSHOT_raspberry-pi_armv6.deb" target="_blank">Raspbian Snapshot Release</a></li>
           </ul>
       </div>
   </div>
@@ -66,9 +66,9 @@
   <p>
     If you want to manually install Kura on your Raspberry Pi or BeagleBone, the jar file archives can be found below.
     <ul>
-        <li style="color: #4f5c6d"><a href="http://www.eclipse.org/downloads/download.php?file=/kura/releases/1.1.1/kura_1.1.1_raspberry-pi.zip" target="_blank">Raspbian</a></li>
-        <li style="color: #4f5c6d"><a href="http://www.eclipse.org/downloads/download.php?file=/kura/releases/1.1.1/kura_1.1.1_raspberry-pi-bplus.zip" target="_blank">Raspbian (Model B+)</a></li>
-        <li style="color: #4f5c6d"><a href="http://www.eclipse.org/downloads/download.php?file=/kura/releases/1.1.1/kura_1.1.1_beaglebone.zip" target="_blank">BeagleBone</a></li>
+        <li style="color: #4f5c6d"><a href="http://www.eclipse.org/downloads/download.php?file=/kura/releases/1.1.2/kura_1.1.2_raspberry-pi.zip" target="_blank">Raspbian</a></li>
+        <li style="color: #4f5c6d"><a href="http://www.eclipse.org/downloads/download.php?file=/kura/releases/1.1.2/kura_1.1.2_raspberry-pi-bplus.zip" target="_blank">Raspbian (Model B+)</a></li>
+        <li style="color: #4f5c6d"><a href="http://www.eclipse.org/downloads/download.php?file=/kura/releases/1.1.2/kura_1.1.2_beaglebone.zip" target="_blank">BeagleBone</a></li>
     </ul>
   </p>
 </div>
