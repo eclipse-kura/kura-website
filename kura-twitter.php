f47d8e7cf20b339639e8a984040e9b64dd065687
David Woodard <david.woodard@eurotech.com>
2015-06-08 14:33:07 -0700

    Update to Kura v1.2.0.

---
+++ kura-twitter.php
@@ -0,0 +1,8 @@
+<?php include('includes/header.php') ?>
+<div class="row">
+    <div class="col-sm-8">
+<a class="twitter-timeline" href="https://twitter.com/search?q=%23Kura%20IoT" data-widget-id="608010945114169347">Tweets about #Kura IoT</a>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+    </div>
+</div>
+<?php include('includes/footer.php') ?>
