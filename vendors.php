fa687838a92d48ad439fe4da746ac3ac45b920c1
David Woodard <david.woodard@eurotech.com>
2017-03-24 18:35:23 -0400

    Clean up vendors page.

--- vendors.php
+++ vendors.php
@@ -2,10 +2,12 @@
 <div class="container" style="min-height: 800px;">
   <h2>Eurotech</h2>
   <p>
-    The below list of Eurotech devices utilize the Kura framework.
+    The below list of <a href="http://www.eurotech.com/en/products/devices/iot+gateways" target="_blank">Eurotech gateways</a> utilize the Kura framework.
     <ul>
-      <li style="color: #4f5c6d"><a href="http://www.eurotech.com/en/products/ReliaGATE%2015-10" target="_blank">Reliagate 15-10</a></li>
+      <li style="color: #4f5c6d"><a href="http://www.eurotech.com/en/products/ReliaGATE%2020-25" target="_blank">Reliagate 20-25</a></li>
       <li style="color: #4f5c6d"><a href="http://www.eurotech.com/en/products/ReliaGATE%2010-20" target="_blank">Reliagate 10-20</a></li>
+       <li style="color: #4f5c6d"><a href="http://www.eurotech.com/en/products/ReliaGATE%2010-11" target="_blank">Reliagate 10-11</a></li>
+       <li style="color: #4f5c6d"><a href="http://www.eurotech.com/en/products/ReliaGATE%2010-05" target="_blank">Reliagate 10-05</a></li>
     </ul>
   </p>
   <h2>Others</h2>
