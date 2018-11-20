3d169fd6f0845a8701fb3c73c01be1788fcdde67
Maiero <matteo.maiero@eurotech.com>
2018-11-20 10:25:50 +0100

    Updated website with references to Docker image.
    Signed-off-by: Maiero <matteo.maiero@eurotech.com>

--- content/en_index.php
+++ content/en_index.php
@@ -122,6 +122,9 @@ setTimeout(function () {
       </p>
       <p><a class="btn btn-info" href="http://eclipse.github.io/kura/intro/raspberry-pi-quick-start.html#eclipse-kuratrade-installation">Install Instructions</a></p>
       <hr>
+      <p><b>Docker</b> run: <b>docker run -d -p 8080:8080 -t eclipse/kura:4.0.0</b></p>
+      <p><a class="btn btn-warning" href="https://github.com/eclipse/kura/tree/develop/kura/container/kura">Documentation</a></p>
+      <hr>
       <p>For other platforms, please refer to the Documentation Page.</p>
       <p><a class="btn btn-warning" href="http://eclipse.github.io/kura/">Documentation</a></p>
     </div>
